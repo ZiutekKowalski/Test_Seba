cde215d2d45737ec4e3cade674fe1e307939538a
cp24 <ziutekkowalskiii@gmail.com>
2019-08-25 15:20:08 +0200

    Initial comit

---
+++ src/main/java/NewOne.java
@@ -0,0 +1,22 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class NewOne {
+    @Given("^user on loading page$")
+    public void userOnLoadingPage() {
+    }
+
+    @When("^user inputs login$")
+    public void userInputsLogin() {
+    }
+
+    @And("^user inputs password$")
+    public void userInputsPassword() {
+    }
+
+    @Then("^login$")
+    public void login() {
+    }
+}
---
+++ src/main/resources/Nowy.feature
@@ -0,0 +1,7 @@
+Feature: jakistamfeature
+
+  Scenario: jakisscenariusz
+    Given user on loading page
+    When user inputs login
+    And user inputs password
+    Then login
